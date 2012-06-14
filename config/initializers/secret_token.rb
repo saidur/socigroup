4852e4afc5f81e69f6d8d0e1a70ad3957d16998c
bijon <bijon@somewherein.net>
2012-06-14 10:28:36 +0600

    Initital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Socigroup::Application.config.secret_token = '04107c726e1c241b23e1d392d86e72da2a0b9b975380baebb7b18198cff16d339be170e97df4ecb5ece8ddcdca50397c5c9316e23a44c1711cc0da664697b0d6'
